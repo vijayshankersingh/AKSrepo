c215713585c4a64674f05b2a75cf6dc2f7a3dd94
VJ-DevOps <vijay.shanker@hotmail.com>
2024-10-09 14:19:23 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,31 +2,31 @@ variable "region" {
   type    = string
   default = "eastus"
   description = " Region where the resource will be created"
-  nullable = false
+  # nullable = false
 }
 
 variable "resourcegroup" {
   type    = string
   default = "resource-rg"
   description = " Resource group where the resource will be created"
-  nullable = false
+  # nullable = false
 }
 
 variable "project" {
   type    = list(string)
   default = ["humana"]
   description = " Project for which the resource will be created"
-  nullable = false
+  # nullable = false
 }
 variable secret {
     type = string
     description = "Secrets for apps"
-    nullable=true
+   # nullable=true
     sensitive=true
 }
 variable userid {
     type = string
     description = "Object id of User to access the AKV"
-    nullable=true
+   # nullable=true
     sensitive=true
 }
